87a4dfaa3f8107da84538ae34a7dacfccb4b330d
c0c05 <alvaropucheg@gmail.com>
2023-04-24 17:38:56 +0200

    funciona con creación de base de datos y sin variables

--- api/app/variables.py
+++
@@ -1,9 +0,0 @@
-import os
-def cargarVariables():
-    os.environ['DB_USERNAME']='root'
-    os.environ['DB_PASSWORD']='1234'
-    os.environ['DB_DATABASE']='db_exploits'
-    os.environ['DB_HOST']='mysql'
-    os.environ['DB_PORT']='3306'
-    os.environ['PORT']='8080'
-    os.environ['HOST']='0.0.0.0'
